bd3138858875a1aeef0387553dfd8bb977e9507f
GiuseppeGarone <giuseppe.garone.1992@gmail.com>
2024-09-08 18:50:28 +0200

    added item and category models

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -1,11 +1,16 @@
+# workaround to display images in development env
+# (remove when in production env!)
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
 from core.views import index, contact
 
-
+# don't add static resources like this in production!
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
