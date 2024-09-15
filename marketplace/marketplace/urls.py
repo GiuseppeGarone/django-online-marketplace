99cc4a794b9ab993455bb7441bd15557fda9b5cb
GiuseppeGarone <giuseppe.garone.1992@gmail.com>
2024-09-15 16:28:43 +0200

    added item detail page

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -4,13 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 
 # don't add static resources like this in production!
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
